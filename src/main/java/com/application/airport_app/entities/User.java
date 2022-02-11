4397826e3d9575df19f406436c93b18c34ab376e
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-02-11 04:55:47 +0100

    added age

--- src/main/java/com/application/airport_app/entities/User.java
+++ src/main/java/com/application/airport_app/entities/User.java
@@ -1,4 +1,28 @@
 package com.application.airport_app.entities;
 
+import javax.persistence.Transient;
+import java.time.LocalDate;
+import java.time.Period;
+
 public class User {
+
+    private LocalDate dateOfBirth;
+    @Transient
+    private Integer age;//should be removed from the constructors
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getAge() {
+        return Period.between(this.dateOfBirth,LocalDate.now()).getYears();
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
